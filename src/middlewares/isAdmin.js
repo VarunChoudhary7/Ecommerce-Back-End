163abe5ddc4fbc2b12b5de7d77fbd21d268b4730
VarunChoudhary7 <varunchoudhary828@gmail.com>
2022-01-22 03:26:16 +0530

    Auth and Admin 2nd

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 
 const isAdmin = (req, res, next) => {
-    const token = req.headers.authorization?.split("")[1]
+    const token = req.headers.authorization?.split(' ')[1]
     console.log(token)
     if (token) {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
