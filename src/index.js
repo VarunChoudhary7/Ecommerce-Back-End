202d3ab58f9614908102a61439f5bf5606b741a5
VarunChoudhary7 <varunchoudhary828@gmail.com>
2022-01-23 19:17:00 +0530

    15th

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./services/mongoDB/connectDB";
 dotenv.config()
-import connectDB from './services/mongodb/connectDB'
-import cors from 'cors'
-import authRoutes from './routes/authRoutes'
-import categoryRoutes from './routes/categoryRoutes'
-import productRoutes from './routes/productRoutes'
-
+import cors from "cors"
+import authRoutes from "./routes/authRoutes"
+import categoryRoutes from "./routes/categoryRoutes"
+import productRoutes from "./routes/productRoutes"
 
 const app = express()
-const port = process.env.PORT || 3003
+const port = process.env.port || 3000
 
 connectDB()
 
 app.use(cors())
 app.use(express.json())
 
-//route to handle auth requests
+//route to handle auth request
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
-app.get('/', (req, res) => {
-    res.send(`Server running at ${port}  `)
-})
 
 app.listen(port, (req, res) => {
-    console.log(`Server listening at PORT ${port}`)
+    console.log(`Server listening at port ${port}`)
 })
