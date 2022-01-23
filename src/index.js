4d469c62b824853290d64c9b4c12cac11274c560
VarunChoudhary7 <varunchoudhary828@gmail.com>
2022-01-23 17:41:16 +0530

    2nd

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
-console.log("hi")
+console.log("hi to you all")
 
 app.listen(port, (req, res) => {
     console.log(`Server listening at port ${port}`)
