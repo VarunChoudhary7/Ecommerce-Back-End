ea460c6911bf39fcfbab0b71bfd8431fae85a2e8
VarunChoudhary7 <varunchoudhary828@gmail.com>
2022-01-23 23:31:08 +0530

    23rd

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ app.use(express.json())
 
 //route to handle auth request
 app.use("/api/v1/auth", authRoutes)
-
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
