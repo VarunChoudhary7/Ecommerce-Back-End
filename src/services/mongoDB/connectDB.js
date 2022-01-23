178d6ce49f441aa75e905333cbbff5e008771ef2
VarunChoudhary7 <varunchoudhary828@gmail.com>
2022-01-23 18:40:09 +0530

    10th

--- src/services/mongoDB/connectDB.js
+++ src/services/mongoDB/connectDB.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
-    const conectionString = process.env.DB_URL
+    const connectionString = process.env.DB_URL
 
     try {
-        await mongoose.connect(conectionString)
+        await mongoose.connect(`${connectionString}`, { useNewUrlParser: true })
         console.log('Connected to the DB !!! ')
     } catch (error) {
         console.log(`could not connect to the DB: ${error.message}`)
