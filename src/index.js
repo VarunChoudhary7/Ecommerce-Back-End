27a5591d6566bfcf301020462a4bb11a47d05b75
VarunChoudhary7 <varunchoudhary828@gmail.com>
2022-01-23 22:11:52 +0530

    17th

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes"
 import productRoutes from "./routes/productRoutes"
 
 const app = express()
+
 const port = process.env.port || 3000
 
 connectDB()
