8a0e3b778a4945c2fa4ed97c610bc5810493ffcc
VarunChoudhary7 <varunchoudhary828@gmail.com>
2022-01-23 19:13:08 +0530

    14th

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-import dotenv from "dotenv"
+import express from 'express'
+import dotenv from 'dotenv'
 dotenv.config()
-import express from "express";
-import connectDB from "./services/mongoDB/connectDB";
+import connectDB from './services/mongodb/connectDB'
+import cors from 'cors'
+import authRoutes from './routes/authRoutes'
+import categoryRoutes from './routes/categoryRoutes'
+import productRoutes from './routes/productRoutes'
 
-import cors from "cors"
-import authRoutes from "./routes/authRoutes"
-import categoryRoutes from "./routes/categoryRoutes"
-import productRoutes from "./routes/productRoutes"
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3003
 
 connectDB()
+
 app.use(cors())
 app.use(express.json())
 
-//route to handle auth request
+//route to handle auth requests
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
-console.log("hi to you all")
+app.get('/', (req, res) => {
+    res.send(`Server running at ${port}  `)
+})
 
 app.listen(port, (req, res) => {
-    console.log(`Server listening at port ${port}`)
+    console.log(`Server listening at PORT ${port}`)
 })
--- src/services/mongoDB/connectDB.js
+++ src/services/mongoDB/connectDB.js
@@ -3,14 +3,14 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
     const connectionString = process.env.DB_URL
 
-    // try {
-    //     await mongoose.connect(connectionString)
-    //     console.log('Connected to the DB !!! ')
-    // } catch (error) {
-    //     console.log(`could not connect to the DB: ${error.message}`)
-    //     console.log(error.message)
-    // }
-    mongoose.connect(connectionString).then(() => { console.log("Connected") }).catch((err) => { console.log(err.message) })
+    try {
+        await mongoose.connect(connectionString)
+        console.log('Connected to the DB !!! ')
+    } catch (error) {
+        console.log(`could not connect to the DB: ${error.message}`)
+        console.log(error.message)
+    }
+    // mongoose.connect(connectionString).then(() => { console.log("Connected") }).catch((err) => { console.log(err.message) })
 }
 
 export default connectDB    
