47736437fb49a5e638aae763e4cc34bf3b22aee3
Varun Choudhary <varunchoudhary828@gmail.com>
2023-06-06 00:47:44 +0530

    cors removed

--- src/index.js
+++ src/index.js
@@ -13,10 +13,11 @@ const port = process.env.PORT || 3003
 
 connectDB()
 
-app.use(cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-}))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 
 app.use(express.json())
 
